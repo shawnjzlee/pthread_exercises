feefcbfad94deac9254ba26303c90ee522cddf67
Shawn Lee <slee208@ucr.edu>
2016-03-29 08:14:19 +0000

    Added work completed so far. Not sure how to continue.

--- src/heat_distribution/heat_distribution.cpp
+++ src/heat_distribution/heat_distribution.cpp
@@ -19,11 +19,16 @@ struct thread_data
     int r_bound;                        /* Contains the right-most column */
     
     /* Variables to check for correctness */
-    int columns;                        /* Number of columns owned by thread. */
+    int columns;                        /* Number of columns in matrix. */
+    int rows;                           /* Number of rows in matrix. */
+    double max_difference;              /* Max difference after updating the
+                                           thread bounds */
 };
 
 /* Globally accessible variables and mutex */
-pthread_mutex_t mutex_cell_update;
+double ** matrix;
+double tolerance;
+pthread_mutex_t mutex_col_update;
 // struct thread_data * thread_data_array;
 
 int thread_get_columns (struct thread_data * data)
@@ -31,18 +36,114 @@ int thread_get_columns (struct thread_data * data)
     return data->r_bound - data->l_bound + 1;
 }
 
+void output_matrix (double ** matrix, int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return;
+}
+
+/* TODO: Computation that updates each cell between the thread's l_bound and
+   r_bound. Ensure that at the l_bound and r_bound, other threads are not
+   accessing the same column(s). Use mutex_col_update to lock the left or
+   right bounded column. */
+   
+/* Boundry cases:
+    1) Top Left
+    2) Bottom Left
+    3) Top Right
+    4) Bottom Right
+    5) Column boundries (will vary depending on column row )
+    */
+void update_cell (struct thread_data * data)
+{
+    double initial = 0, diff = 0;
+    /* Traverse top-bottom */
+    for (int i = 0; i < data->rows; i++)
+    {
+        for (int j = 0; j < data->columns; j++)
+        {
+            initial = matrix[i][j];
+            
+            /* else if ceiling */
+            if (i == 0)
+            {
+                if (j == 0)
+                    matrix[i][j] = ((matrix[i+1][j] + matrix[i][j+1]) / 2 );
+                else
+                {
+                    matrix[i][j] = ((matrix[i][j-1] + matrix[i+1][j] 
+                                     + matrix[i][j+1]) / 3);
+                }
+            }
+            /* else if left wall */
+            else if (j == 0)                     
+            {
+                if (i == data->rows - 1)
+                    matrix[i][j] = ((matrix[i-1][j] + matrix[i][j+1]) / 2 );
+                else
+                {
+                    matrix[i][j] = ((matrix[i+1][j] + matrix[i-1][j] 
+                                     + matrix[i][j+1]) / 3 );
+                }
+            }
+            /* else if floor */
+            else if (i == data->rows - 1)
+            {
+                if (j == data->columns - 1)
+                    matrix[i][j] = ((matrix[i-1][j] + matrix[i][j-1]) / 2 );
+                else
+                {
+                    matrix[i][j] = ((matrix[i][j-1] + matrix[i-1][j] 
+                                     + matrix[i][j+1]) / 3);
+                }
+            }
+            /* else if right wall */
+            else if (j == data->columns - 1)         
+            {
+                if (i == 0)
+                    matrix[i][j] = ((matrix[i+1][j] + matrix[i][j-1]) / 2 );
+                else
+                {
+                    matrix[i][j] = ((matrix[i+1][j] + matrix[i][j-1] 
+                                     + matrix[i-1][j]) / 3 );
+                }
+            }
+            else
+            {
+                matrix[i][j] = ((matrix[i-1][j] + matrix [i][j-1] 
+                                 + matrix [i][j+1] + matrix [i+1][j])/4);
+            }
+            diff = matrix[i][j] - initial;
+            if (diff > data->max_difference)
+            {
+                data->max_difference = diff;
+            }
+        }
+    }
+}
+
 void * update_matrix(void * threadarg)
 {
     short tid = 0;
-    int columns = 0;
     struct thread_data * data;
     
     data = (struct thread_data *) threadarg;
     tid = data->thread_id;
-    data->columns = thread_get_columns(data);
     
     printf("#%hi owns %i columns, with left at %i and right at %i.\n"
-           , tid, data->columns, data->l_bound, data->r_bound);
+           , tid, thread_get_columns(data), data->l_bound, data->r_bound);
+    
+    // do
+    // {
+        update_cell (data);
+    // } while (data->max_difference > tolerance);
     
     pthread_exit(NULL);
 }
@@ -73,8 +174,8 @@ int main(int argc, char * argv[])
     // bool divisible = true;
     
     /* Main() matrix attributes */
-    double top_left, top_right, bottom_left, bottom_right, tolerance = 0.0;
-    int row, column = 0;
+    double top_left, top_right, bottom_left, bottom_right;
+    int row , column  = 0;
 
     /* Input file string */
     string input_file;
@@ -118,36 +219,38 @@ int main(int argc, char * argv[])
     struct thread_data thread_data_array[num_threads];
     
     /* Get all matrix related variables from input file */
-    instream >> row >> column
+    instream >> row  >> column 
              >> top_left >> top_right 
              >> bottom_left >> bottom_right 
              >> tolerance;
 
-    /* Initialize the 2D array with the above initial values */
-    double matrix[row][column];
+    /* Initialize the 2D array with the above initial values (H X W)*/
+    matrix = new double * [row];
+    for(i = 0; i < row ; ++i)
+        matrix[i] = new double [column];
     matrix[0][0] = top_left;
     matrix[0][column - 1] = top_right;
     matrix[row - 1][0] = bottom_left;
     matrix[row - 1][column - 1] = bottom_right;
-
+    
     /* Partition the number of columns per thread. If there are too many
        requested threads, set it to the number of columns. Remaining number 
        of columns will be distributed evenly to the latter threads. */
     if (num_threads > column)
         num_threads = column;
-    if (column % num_threads)
-        remaining_cols = column % (num_threads);
+    if (column  % num_threads)
+        remaining_cols = column  % (num_threads);
     cols_per_thread = column / (num_threads);
     
     // cout << "Columns per thread (Ext + 1): " << cols_per_thread << endl;
     // cout << "Remaining cols: " << remaining_cols << endl;
     // cout << "Number of threads: " << num_threads << endl;
-    // cout << "Rows / Columns: " << row << " / " << column << endl << endl;
+    // cout << "Rows / Columns: " << column << " / " << column << endl << endl;
 
     /* Create the array of threads */
     pthread_t threads[num_threads];
     /* Initialize the mutex */
-    pthread_mutex_init (&mutex_cell_update, NULL);
+    pthread_mutex_init (&mutex_col_update, NULL);
     /* Initialize and set thread detached attribute */
     pthread_attr_t attr;
     pthread_attr_init(&attr);
@@ -163,7 +266,7 @@ int main(int argc, char * argv[])
     int num_norm_cols = (column - (column % num_threads));
     int num_ext_cols = column - num_norm_cols;
 
-    for (; i < num_norm_cols, index < num_threads - remaining_cols; 
+    for (i = 0; i < num_norm_cols, index < num_threads - remaining_cols; 
            i += normal_dist, index++)
     {
         cout << "In main: creating thread " << index << endl;
@@ -181,6 +284,8 @@ int main(int argc, char * argv[])
         thread_data_array[index].l_thread = r_thread;
         thread_data_array[index].l_bound = i;
         thread_data_array[index].r_bound = i + normal_dist - 1;
+        thread_data_array[index].columns = column;
+        thread_data_array[index].rows = row;
         rc = pthread_create(&threads[index], &attr, 
                             update_matrix, (void *) &thread_data_array[index]);
         if(rc)
@@ -191,7 +296,7 @@ int main(int argc, char * argv[])
     }
     /* Sets columns for extended distribution of columns 
        TODO: Reduce the number of iterators in the loop, if possible */
-    for (; j < num_ext_cols; i += ext_dist, j++, index++)
+    for (j = 0; j < num_ext_cols; i += ext_dist, j++, index++)
     {
         cout << "In main: creating thread " << index << endl;
         
@@ -208,6 +313,8 @@ int main(int argc, char * argv[])
         thread_data_array[index].l_thread = r_thread;
         thread_data_array[index].l_bound = i;
         thread_data_array[index].r_bound = i + ext_dist - 1;
+        thread_data_array[index].columns = column;
+        thread_data_array[index].rows = row;
         rc = pthread_create(&threads[index], &attr, 
                             update_matrix, (void *) &thread_data_array[index]);
         if(rc)
@@ -219,5 +326,9 @@ int main(int argc, char * argv[])
     
     /* Last thing that main() should do */
     printf("main(): program completed. \n");
+    output_matrix (matrix, row, column);
+    for (i = 0; i < row; ++i)
+        delete [] matrix[i];
+    delete [] matrix;
     pthread_exit(NULL);
 }
